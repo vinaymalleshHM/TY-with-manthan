3499d0cf8a210b54aba8a61e07e4d823a5c0c384
vinay <princevinay404@gmail.com>
2020-04-01 00:34:34 +0530

    s

---
+++ Vinay Mallesh H M -Final/ecs/Wrapper/src/com/testyantra/wrapper/test/TestA.java
@@ -0,0 +1,18 @@
+package com.testyantra.wrapper.test;
+
+public class TestA {
+	public static void main(String[] args) {
+		
+		
+		Integer k = new Integer(25);
+		
+		int i=k;// auto-unboxing
+		
+		System.out.println(i);
+		
+		int r =90;
+		Integer p = r;//auto-boxing
+		System.out.println(p);
+	}
+
+}

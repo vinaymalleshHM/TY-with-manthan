e4f80576bcb78492d5182acd679329a12027baa3
vinay <princevinay404@gmail.com>
2020-04-01 00:15:34 +0530

    s

---
+++ TestYantra/Vinay Mallesh H M/Java/Wrapper/src/com/testyantra/wrapper/test/TsetB.java
@@ -0,0 +1,35 @@
+package com.testyantra.wrapper.test;
+
+public class TsetB {
+	public static void main(String[] args) {
+		String a = "2.4";
+		
+		String b = "25";
+		
+		String c = "2.4";
+		
+		String d = "gh";
+		
+		
+		int i = Integer.parseInt(a);
+		
+		int j = Integer.parseInt(b);
+		
+		double k = Double.parseDouble(c);
+		
+		boolean m = Boolean.parseBoolean(d);
+		
+		
+		System.out.println(a+b);
+		
+		
+		System.out.println(i+j);
+		
+		
+		System.out.println(k*2);
+		
+		
+		System.out.println(m);
+}
+
+}

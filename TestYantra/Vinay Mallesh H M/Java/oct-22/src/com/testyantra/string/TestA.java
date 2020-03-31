13ca64dcf01db45388cd142e168ad77058fb0c7e
vinay <princevinay404@gmail.com>
2020-04-01 00:12:39 +0530

    s

---
+++ TestYantra/Vinay Mallesh H M/Java/oct-22/src/com/testyantra/string/TestA.java
@@ -0,0 +1,39 @@
+package com.testyantra.string;
+
+public class TestA {
+	public static void main(String[] args) {
+		
+		
+		/*String k = "raju";
+		int l = k.length();
+		//String s = new String();
+		//System.out.println(l);*/
+		
+		
+		/*String t = "priya";
+		
+		String p = "Priya";
+		
+		System.out.println(t.equalsIgnoreCase(p));*/
+		
+		
+		String t = "dimple  ";
+		
+		String y = t.toUpperCase();
+		
+		System.out.println("t is "+t);
+		
+		System.out.println("y is "+y);
+		
+		System.out.println(t.toLowerCase());
+		
+		System.out.println(t.trim());
+		
+		System.out.println(t.charAt(2));
+		
+		//System.out.println(split());
+		
+		
+	}
+
+}

1c8bb21133f1ae22d6e4b1309b6d5faa0e1f51e6
vinay <princevinay404@gmail.com>
2020-04-01 00:32:08 +0530

    s

---
+++ Vinay Mallesh H M -Final/ecs/oct-22/src/com/testyantra/string/TestB.java
@@ -0,0 +1,29 @@
+package com.testyantra.string;
+
+public final class TestB {
+	
+	int i = 23;
+	public static void main(String[] args) {
+		
+	/*	
+		StringBuffer sb = new StringBuffer("raju");
+		
+		StringBuffer sb1 = new StringBuffer();
+		sb1.append("dimple");
+		
+		
+		StringBuilder sbu = new StringBuilder();
+		
+		StringBuilder sbu1 = new StringBuilder();
+		sbu1.append("i am ");
+		
+		*/
+		
+		Student s =new Student("ty", 2);
+		
+		
+		
+		
+
+}
+}
